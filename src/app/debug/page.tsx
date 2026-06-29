0f7b0ce93ae029ac567aa2a4093e0ddd16707a69
begiedz <dariusz.begiedza@gmail.com>
2026-06-29 19:43:03 +0200

    feat: add debug site prod env guard

--- src/app/debug/page.tsx
+++ src/app/debug/page.tsx
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 import { TextLockTester } from "@/components/lock-debug/LockTester";
 import { textLockCopy } from "../textLockCopy";
 
 export default function Debug() {
+  if (process.env.NODE_ENV === "production") {
+    notFound();
+  }
   return <TextLockTester copy={textLockCopy} />;
 }
