731b3d8aec0811b8ff085c8c3e279ad29d336bd2
begiedz <dariusz.begiedza@gmail.com>
2026-06-29 22:13:13 +0200

    feat: update icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import Button from "@/components/general/atoms/button";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <Button>
-        <a href="/game">Play</a>
-      </Button>
+      <Link
+        href="/game"
+        className="flex gap-2 hover:bg-foreground p-3 border border-foreground font-medium text-foreground hover:text-background tracking-wide transition-all"
+      >
+        Play
+      </Link>
     </main>
   );
 }
