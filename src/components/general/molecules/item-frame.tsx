492119e5f440eb81aeca2a83c78bb668887e1dab
begiedz <dariusz.begiedza@gmail.com>
2026-06-29 15:42:36 +0200

    feat: add item frame component

---
+++ src/components/general/molecules/item-frame.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+interface ItemFrameProps {
+  className?: string;
+  item: string;
+  amount?: number;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+export default function ItemFrame({
+  className,
+  item,
+  amount = 0,
+  width,
+  height,
+  alt,
+}: ItemFrameProps) {
+  return (
+    <div
+      className={cn(
+        "relative bg-background border border-foreground aspect-square",
+        className,
+      )}
+    >
+      <Image
+        src={item}
+        width={width}
+        height={height}
+        alt={alt}
+        className="-translate-y-2"
+      />
+      <div className="right-1 bottom-1 absolute bg-foreground p-1 aspect-square">
+        <span className="font-heading text-background">{amount}</span>
+      </div>
+    </div>
+  );
+}
