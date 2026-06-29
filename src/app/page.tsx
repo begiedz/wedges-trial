7b36e03e288ead074e4a3af4692505a18259a246
begiedz <dariusz.begiedza@gmail.com>
2026-06-29 16:37:40 +0200

    chore: update homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Button from "@/components/general/atoms/button";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <main className="flex flex-col items-center">
+      <a href="/game">
+        <Button>Play</Button>
+      </a>
+    </main>
+  );
 }
--- src/components/general/molecules/header.tsx
+++ src/components/general/molecules/header.tsx
@@ -3,7 +3,9 @@ import Logo from "../atoms/logo";
 export default function Header() {
   return (
     <header className="flex justify-center p-2">
-      <Logo />
+      <a href="/">
+        <Logo />
+      </a>
     </header>
   );
 }
